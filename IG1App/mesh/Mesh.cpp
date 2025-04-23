a0cfc84bd90d8f72f77f31ce2fe43aba199f976b
ChichoRD <chichord2005@hotmail.com>
2025-04-23 19:07:25 +0200

    feat: ex57

--- IG1App/mesh/Mesh.cpp
+++ IG1App/mesh/Mesh.cpp
@@ -54,6 +54,14 @@ Mesh::load()
 			glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(vec4), nullptr);
 			glEnableVertexAttribArray(1);
 		}
+
+		if (vNormals.size() > 0) { // upload normals
+			glGenBuffers(1, &mNBO);
+			glBindBuffer(GL_ARRAY_BUFFER, mNBO);
+			glBufferData(GL_ARRAY_BUFFER, vNormals.size() * sizeof(vec3), vNormals.data(), GL_STATIC_DRAW);
+			glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vec3), nullptr);
+			glEnableVertexAttribArray(2);
+		}
 	}
 }
 
@@ -69,6 +77,11 @@ Mesh::unload()
 		if (mCBO != NONE) {
 			glDeleteBuffers(1, &mCBO);
 			mCBO = NONE;
+		}
+
+		if (mNBO != NONE) {
+			glDeleteBuffers(1, &mNBO);
+			mNBO = NONE;
 		}
 	}
 }
