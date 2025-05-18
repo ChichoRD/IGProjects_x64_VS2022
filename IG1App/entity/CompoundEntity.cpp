8298d05fe5a22ab6b91f5b8af3f0bbbf932dad61
ChichoRD <chichord2005@hotmail.com>
2025-05-19 00:04:28 +0200

    fix: compound entity memory leak

--- IG1App/entity/CompoundEntity.cpp
+++ IG1App/entity/CompoundEntity.cpp
@@ -1,25 +1,59 @@
 #include "CompoundEntity.h"
+#include <type_traits>
+#include <cassert>
+#include <cstdlib>
+
+void CompoundEntity::addEntity(Abs_Entity* ae) {
+	if (ae == nullptr) {
+		assert(false && "error: entity may not be null");
+		std::exit(EXIT_FAILURE);
+	}
+
+	std::unique_ptr<Abs_Entity> entity{ ae };
+	gObjects.push_back(std::move(entity));
+}
+
+size_t CompoundEntity::child_count() const {
+	return gObjects.size();
+}
+
+const Abs_Entity& CompoundEntity::get_child(const size_t index) const {
+	assert(
+		index < gObjects.size()
+		&& "fatal error: child index out of bounds"
+	);
+	return *gObjects.at(index);
+}
+
+Abs_Entity& CompoundEntity::get_child(const size_t index) {
+	assert(
+		index < gObjects.size()
+		&& "fatal error: child index out of bounds"
+	);
+	return *gObjects.at(index);
+}
+
 
 void CompoundEntity::render(const glm::mat4& basis) const {
-	for (Abs_Entity* entity : gObjects) {
+	for (const std::unique_ptr<Abs_Entity> &entity : gObjects) {
 		entity->render(basis * mModelMat);
 	}
 }
 
 void CompoundEntity::update(double time_seconds, double delta_time_seconds) {
-	for (Abs_Entity* entity : gObjects) {
+	for (std::unique_ptr<Abs_Entity>& entity : gObjects) {
 		entity->update(time_seconds, delta_time_seconds);
 	}
 }
 
 void CompoundEntity::load() {
-	for (Abs_Entity* entity : gObjects) {
+	for (std::unique_ptr<Abs_Entity> &entity : gObjects) {
 		entity->load();
 	}
 }
 
 void CompoundEntity::unload() {
-	for (Abs_Entity* entity : gObjects) {
+	for (std::unique_ptr<Abs_Entity> &entity : gObjects) {
 		entity->unload();
 	}
 }
--- IG1App/entity/CompoundEntity.h
+++ IG1App/entity/CompoundEntity.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Entity.h"
 #include <cassert>
+#include <memory>
 
 class CompoundEntity : public Abs_Entity
 {
@@ -8,22 +9,10 @@ public:
 	CompoundEntity() : Abs_Entity() {}
 	~CompoundEntity() {};
 
-	inline void addEntity(Abs_Entity* ae) { gObjects.push_back(ae); }
-	inline size_t child_count() const { return gObjects.size(); }
-	inline const Abs_Entity& get_child(const size_t index) const {
-		assert(
-			index < gObjects.size()
-			&& "fatal error: child index out of bounds"
-		);
-		return *gObjects.at(index);
-	}
-	inline Abs_Entity& get_child(const size_t index) {
-		assert(
-			index < gObjects.size()
-			&& "fatal error: child index out of bounds"
-		);
-		return *gObjects.at(index);
-	}
+	void addEntity(Abs_Entity* ae);
+	size_t child_count() const;
+	const Abs_Entity& get_child(const size_t index) const;
+	Abs_Entity& get_child(const size_t index);
 
 	void render(const glm::mat4& modelViewMat) const override;
 	void update(double time_seconds, double delta_time_seconds) override;
@@ -31,6 +20,6 @@ public:
 	void unload();
 
 private:
-	std::vector<Abs_Entity*> gObjects;
+	std::vector<std::unique_ptr<Abs_Entity>> gObjects;
 };
 
